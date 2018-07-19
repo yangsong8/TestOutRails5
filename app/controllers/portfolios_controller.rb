fb5e7ba46015a233efef70b402cfbecca9aa7468
yangsong8 <ysong8@ncsu.edu>
2018-07-19 13:56:38 -0400

    Add new and create functions for portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -2,4 +2,26 @@ class PortfoliosController < ApplicationController
   def index
     @portfolio_items = Portfolio.all
   end
+
+  def new
+    @portfolio_items = Portfolio.new
+  end
+
+  def create
+    @portfolio_items = Portfolio.new(portfolio_params)
+    respond_to do |format|
+      if @portfolio_items.save
+        # instead of sending users to the show page, bring them to the full list of portfolio items
+        format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  private
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def portfolio_params
+    params.require(:portfolio).permit(:title, :subtitle, :body, :main_image, :thumb_image)
+  end
 end
