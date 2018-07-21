4f56afbcacb35d45b8355c040ba51a667b711859
yangsong8 <ysong8@ncsu.edu>
2018-07-20 23:33:17 -0400

    Configured custom routes for pages_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   resources :portfolios
-  get 'pages/home'
-  get 'pages/about'
-  get 'pages/contact'
+
+  get 'about', to: 'pages#about'
+  get 'contact_admin', to: 'pages#contact'
   resources :blogs
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'pages#home'
 end
