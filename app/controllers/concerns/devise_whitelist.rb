c4ff43ec75acf5951ea59c8bb81d1b043f56a5bf
yangsong8 <ysong8@ncsu.edu>
2018-07-29 16:39:45 -0400

    Added new parameter "name" for user registration

---
+++ app/controllers/concerns/devise_whitelist.rb
@@ -0,0 +1,12 @@
+module DeviseWhitelist
+  extend ActiveSupport::Concern
+  included do
+    before_action :configure_permitted_parameters, if: :devise_controller?
+  end
+
+  def configure_permitted_parameters
+    # only need to add additional fields, default fields, such as email, do not have to be included
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
+end
