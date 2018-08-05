20cac0dcb343881a4139aaeefba01effd7ff3cc0
yangsong8 <ysong8@ncsu.edu>
2018-08-05 15:07:56 -0400

    Refactor the logic for guest user (b4 logging in)

--- app/controllers/concerns/current_user_concern.rb
+++ app/controllers/concerns/current_user_concern.rb
@@ -5,12 +5,13 @@ module CurrentUserConcern
 # try current_user from devise, if it is nil, we will create a guest user obj.
     super || guest_user
   end
+
   def guest_user
-    # create a mini structure for guest user
-    OpenStruct.new(name: "Guest User",
-                   first_name: "Guest",
-                   last_name: "User",
-                   email: "guest@example.com"
-    )
+    guest = GuestUser.new
+    guest.name = "Guest User"
+    guest.first_name = "Guest"
+    guest.last_name = "User"
+    guest.email = "guest@example.com"
+    guest
   end
 end
